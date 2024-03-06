998d6d99346bc449aefc4b95e91d36846a1b4ff0
MonishaKH <160154214+MonishaKH@users.noreply.github.com>
2024-03-06 22:26:05 +0530

    Add files via upload

---
+++ QR_code.py
@@ -0,0 +1,23 @@
+import qrcode
+
+def generate_qrcode(data, file_name):
+    
+    qr = qrcode.QRCode(
+        version=3,
+        box_size=20,
+        border=2,
+        error_correction=qrcode.constants.ERROR_CORRECT_H
+    )
+    
+    qr.add_data(data)
+    qr.make(fit=True)
+    
+    img = qr.make_image(fill_color='black', back_color='white')
+    img.save('QR Code Generator\\' + file_name + '.png')
+    
+
+if __name__ == '__main__':   
+    data = input('Enter an Data: ')
+    file_name = input('Enter File Name: ')
+    
+    generate_qrcode(data, file_name)
